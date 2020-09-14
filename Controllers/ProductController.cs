6fdd71de7e3263efa924af2e28b870ac146ecc49
Dave Gedert <DaveGedert@QuickenLoans.com>
2020-09-13 23:16:08 -0400

    First commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using CoffeeShopLab13_2.Models;
+
+namespace CoffeeShopLab13_2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            IDbConnection db = new SqlConnection("Server=6497Q13\\SQLEXPRESS;Database=CoffeeLab;user id=TestUser;password=password");
+            db.Open();
+
+            List<Products> prods = db.Query<Products>("SELECT * FROM Product").AsList<Products>();
+
+            db.Close();
+            return View(prods);
+        }
+
+        public IActionResult Drink()
+        {
+            IDbConnection db = new SqlConnection("Server=6497Q13\\SQLEXPRESS;Database=CoffeeLab;user id=TestUser;password=password");
+            db.Open();
+
+            List<Products> prods = db.Query<Products>("SELECT * FROM Product").AsList<Products>();
+
+            db.Close();
+            return View(prods);
+        }
+
+        public IActionResult Products(int ID)
+        {
+            IDbConnection db = new SqlConnection("Server=6497Q13\\SQLEXPRESS;Database=CoffeeLab;user id=TestUser;password=password");
+            db.Open();
+
+            Products prod = db.QuerySingle<Products>($"SELECT * FROM Product WHERE ID = {ID}");
+
+            db.Close();
+            return View(prod);
+        }
+
+        public IActionResult Results()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopLab13_2.Models
+{
+    public class Products
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
