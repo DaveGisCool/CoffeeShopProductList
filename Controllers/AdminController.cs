10f884b422b446593b49bfbf2628d919f159827d
Dave Gedert <DaveGedert@QuickenLoans.com>
2020-09-15 07:01:50 -0400

    CRUD Update 1

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using CoffeeShopLab13_2.Models;
+
+namespace CoffeeShopLab13_2.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            IDbConnection db = new SqlConnection("Server=6497Q13\\SQLEXPRESS;Database=CoffeeLab;user id=TestUser;password=password");
+            db.Open();
+
+            List<Products> prods = db.Query<Products>("SELECT * FROM Product").AsList<Products>();
+
+            db.Close();
+            return View(prods);
+        }
+
+        public IActionResult Edit(long _ID)
+        {
+            IDbConnection db = new SqlConnection("Server=6497Q13\\SQLEXPRESS;Database=CoffeeLab;user id=TestUser;password=password");
+            db.Open();
+
+            
+
+            db.Close();
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            IDbConnection db = new SqlConnection("Server=6497Q13\\SQLEXPRESS;Database=CoffeeLab;user id=TestUser;password=password");
+            db.Open();
+
+            
+
+            db.Close();
+            return View();
+        }
+    }
+}
--- Models/Products.cs
+++ Models/Products.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper.Contrib.Extensions;
 
 namespace CoffeeShopLab13_2.Models
 {
+    [Table("Product")]
+
     public class Products
     {
-        public int ID { get; set; }
+        [Key]
+        public long ID { get; set; }
+
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
+
+
+        public static Products Create(string _name, Decimal _price)
+        {
+            // CREATE the new Product instance
+            Products prod = new Products() { Name = _name, Price = _price };
+
+            // SAVE the instance to the database
+            IDbConnection db = new SqlConnection("Server=6497Q13\\SQLEXPRESS;Database=CoffeeLab;user id=TestUser;password=password");
+            long _ID = db.Insert<Products>(prod);
+
+            // SET the id column of the instance
+            prod.ID = _ID;
+
+            // RETURN the instance
+            return prod;
+        }
+
+        public static void Delete(long _ID)
+        {
+            IDbConnection db = new SqlConnection("Server=6497Q13\\SQLEXPRESS;Database=CoffeeLab;user id=TestUser;password=password");
+            db.Delete(new Products() { ID = _ID });
+        }
     }
 }
